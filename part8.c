5596c36d8bab6e5f256094134c1df348eab7c681
Leminhson06 <leminhson3102006@gmail.com>
2025-06-21 11:09:21 +0700

    Add files via upload

--- part8.c
+++ part8.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main () {
+int main() {
+    int a, b;
+    printf("Nhap so nguyen duong thu nhat: ");
+    scanf("%d", &a);
+    printf("Nhap so nguyen duong thu hai: ");
+    scanf("%d", &b);
+    if (a <= 0 || b <= 0) {
+        printf("Vui long nhap hai so nguyen duong!\n");
+        return 1;
+    }
+    int temp_a = a, temp_b = b;
+    while (temp_b != 0) {
+        int temp = temp_b;
+        temp_b = temp_a % temp_b;
+        temp_a = temp;
+    }
+    
+    int result = (a * b) / temp_a;
+    printf("Boi chung nho nhat cua %d va %d la: %d\n", a, b, result);
     
     return 0;
 }
